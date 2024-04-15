765c5f2c7890674fbbb838f23a7be18f44c7480c
luqitas2 <lrh8798@nyu.edu>
2024-04-14 21:55:38 -0400

    added loop

--- ledBlink.ino
+++ ledBlink.ino
@@ -7,4 +7,10 @@ void setup() {
 }
 
 void loop() {
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  delay(300);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, LOW);
+  delay(100);
 }
